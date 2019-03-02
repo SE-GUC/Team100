bb2a8ed63602e6c918b000fe91aa852258b0a747
montasserr <47507331+montasserr@users.noreply.github.com>
2019-03-02 14:47:50 +0200

    Add files via upload
    events

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-const express = require('express')
-
-const committe = require('./committee')
-
-const app = express()
+const express = require('express');
+//const events = require('./routes/api/events');
+const events = require('./events')
+const app = express();
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send(`<h1>Welcome to MUN</h1>
-    `);
-})
+app.use('/events', events)
 
-// Direct routes to appropriate files 
-app.use('/api/committee', committe)
-
-//Handling 404
 app.use((req, res) => {
     res.status(404).send({err: 'We can not find what you are looking for'});
- })
+})
 
-const port = 5000
-app.listen(port, () => console.log(`Server up and running on port ${port}`))
+const port = process.env.PORT | 5000
+app.listen(port, () => console.log(`Listening on port ${port}`));
