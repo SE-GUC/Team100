27584e0ab8396576576712ff0a5a145ab89903a9
mariamsalah1199 <47285437+mariamsalah1199@users.noreply.github.com>
2019-03-02 17:13:49 +0200

    Update committee.js

--- routes/api/committee.js
+++ routes/api/committee.js
@@ -27,6 +27,14 @@ const committee = [
   }
 ];
 
+//view committees
+
+router.get('/view_committee/', (req, res) => {
+   // const id=req.params.id
+     const name = req.body.name
+     const r = committee.find(committee => committee.name === name)
+    res.send(r)
+})
 
 //get all committees
 
