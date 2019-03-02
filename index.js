e1852c8a621bb300e0750445d0b8f1e28a2597e0
dina-elbeih <47152633+dina-elbeih@users.noreply.github.com>
2019-03-02 12:27:36 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,18 @@
 const express = require('express')
+// Create the app
 const app = express()
+// Use it with post
 app.use(express.json())
-const photo = require('./photo')
-const FAQs = require('./FAQs')
-const committe = require('./committee')
-const MissionAndVision = require('./MissionAndVision')
-const Rating = require('./Rating')
+const albums = require('./routes/api/albums') //fo2 ka2eny ba import
 
 
-const messages = require('./messages')
-const team_members = require('./team_members')
+app.use('/api/albums', albums) //ta7t
 
-
-//const router = express.Router();
-//const Joi = require('joi');
-
-// Direct routes to appropriate files 
-app.use('/messages', messages)
-app.use('/team_members', team_members)
-app.use('/MissionAndVision', MissionAndVision)
-app.use('/Rating', Rating)
-
-
-
-//const committe = require('./committee')
-
-app.get('/', (req, res) => {
-    res.send(`<h1>Welcome to MUN</h1>  `);
+app.use((req,res) => {
+    res.status(404).send({err:'We can not find what you are looking for'});
 })
-app.use('/api/photo', photo)
-app.use('/api/FAQs', FAQs)
-app.use('/api/committee', committe)
 
-app.use((req, res) => {
-    res.status(404).send({err: 'We can not find what you are looking for'});
- })
-const port = 5000
+const port = process.env.PORT | 5000
 app.listen(port, () => console.log(`Server up and running on port ${port}`))
+ 
 
