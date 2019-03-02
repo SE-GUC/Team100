1e481e02190b09394ac347334225ad3259499fed
yomna-sherif <47187371+yomna-sherif@users.noreply.github.com>
2019-03-02 13:53:24 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,34 +3,16 @@ const app = express()
 app.use(express.json())
 
 const users = require('./routes/api/users')
-const albums = require('./routes/api/albums')
-const photo = require('./photo')
-const FAQs = require('./FAQs')
-const committe = require('./committee')
-const MissionAndVision = require('./MissionAndVision')
-const Rating = require('./Rating')
-const messages = require('./messages')
-const team_members = require('./team_members')
-
-
-app.use('/messages', messages)
-app.use('/team_members', team_members)
-app.use('/MissionAndVision', MissionAndVision)
-app.use('/Rating', Rating)
 app.use('/users', users)
-app.use('/api/photo', photo)
-app.use('/api/FAQs', FAQs)
-app.use('/api/committee', committe)
-app.use('/api/albums', albums)
 
 
 app.get('/', (req, res) => {
-    res.send(`<h1>Welcome to AWG Hub</h1>  `);
+    res.send(`<h1>Welcome to MUN</h1>  `);
 })
+
 
 app.use((req, res) => {
     res.status(404).send({err: 'We can not find what you are looking for'});
  })
 const port = 5000
 app.listen(port, () => console.log(`Server up and running on port ${port}`))
-
