7d7e35495cfbc5611990aeda5adbb52626a8a6e5
yasminhegazy <47330999+yasminhegazy@users.noreply.github.com>
2019-03-02 17:17:24 +0200

    Update announcements.js

--- routes/api/announcements.js
+++ routes/api/announcements.js
@@ -28,4 +28,61 @@ router.put('/announcements_update/:id', (req, res) => {
 }
 )
 
+router.post('/', (req, res) => {
+    const id = req.body.id
+    const description = req.body.description
+    const date = req.body.date
+    const tag = req.body.tag
+    const created_by = req.body.created_by
+    const videos = req.body.videos
+    const photos = req.body.photos
+    
+    const schema = {
+        description :Joi.string().required(),
+        tag :Joi.string().required(),
+        created_by : Joi.string().required(),
+        date : Joi.date().required(),
+        photos : Joi.string().required(),
+        videos : Joi.string().required(),
+    };
+
+    const result = Joi.validate(req.body, schema);
+    if (result.error) 
+    return res.status(400).send({error: result.error.details[0].message});
+  
+    const announcement = {
+        id: announcements.length +1,
+        description: description,
+        date: date,
+        tag: tag,
+        created_by: created_by,
+        videos: videos,
+        photos: photos,
+     }
+     announcements.push(announcement)
+     return res.json({data: announcements});
+
+     //res.send(announcements)
+})
+
+// read an announcement
+router.get('/', (req, res) => {
+    res.send(announcements)
+})
+
+// Delete an announcement 
+router.delete('/:id', (req, res) => {
+    const announcementID = req.params.id
+
+    const announcement = announcements.find(announcement => announcement.id === announcementID)
+    const index = announcements.indexOf(announcement)
+    if (announcement && index !== null){
+    announcements.splice(index,1)
+    res.send(announcements)
+    }
+    else{
+        res.status(400).send({ err: 'Invalid value for announcement id'});
+    }
+})
+
 module.exports=router
