c15db04c2f7cecb5deaf450b9ef2ac26969d6b0d
yasminhegazy <yasmin.abdelrahman@student.guc.edu.eg>
2019-03-20 15:03:49 +0200

    photo methods added

--- routes/api/photo.js
+++ routes/api/photo.js
@@ -32,4 +32,31 @@ router.post("/", (req, res) => {
     res.send(photo);
   });
   
+router.get('/view_photo/:id', (req, res) => {
+  const id = req.params.id
+  const s = photo.find(photo => photo.id === id)
+  res.send(s)
+})
+
+//an admin should be able to update description about photos 
+router.put('/update_photodesc/:id', (req, res) => {
+  const photoId = req.params.id 
+  const updated_description = req.body.description
+  const p = photo.find(photo => photo.id === photoId)
+  p.description = updated_description
+  res.send(p)
+})
+
+//an admin should be able to delete photos
+router.delete('/delete_photo/:id', (req, res) => {
+  const pid = req.params.id 
+  const p = photo.find(photo => photo.id === pid)
+  const index = photo.indexOf(p)
+  if(p!== null && index!== null){
+  photo.splice(index,1)}
+  res.send(photo)
+})
+
+module.exports=router
+  
   
