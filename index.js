22fb3880738a99dc485c7ff5532782553b018084
Dohakaly <doha.aly@student.guc.edu.eg>
2019-03-23 14:32:52 +0200

    Index with achievements

--- index.js
+++ index.js
@@ -1,50 +1,17 @@
-//
-
 const express = require("express")
 const mongoose = require("mongoose")
-const app = express()
-app.use(express.json())
 
-const events = require("./routes/api/events")
-const users = require("./routes/api/users")
-const albums = require("./routes/api/albums")
-const committee = require("./routes/api/committees")
-const FAQs = require("./routes/api/faqs")
-const photo = require("./routes/api/photo")
-const descriptions = require("./routes/api/descriptions")
-const MissionAndVision = require("./routes/api/missions_visions")
-const Rating = require("./routes/api/ratings")
-const messages = require("./routes/api/messages")
-const team_members = require("./routes/api/team_members")
+// Require Router Handlers
 const achievements = require("./routes/api/achievements")
-const feedback = require("./routes/api/feedbacks")
-const library = require("./routes/api/libraries")
-const subscribers = require("./routes/api/subscribers")
-const announcements = require("./routes/api/announcements")
-const brief_description = require("./routes/api/brief_descriptions")
+//const users = require("./routes/api/users")
+const clubmuns = require("./routes/api/clubmuns")
 
-app.use("/api/events", events)
-app.use("/api/MissionAndVision", MissionAndVision)
-app.use("/api/Rating", Rating)
-app.use("/api/users", users)
-app.use("/api/photo", photo)
-app.use("/api/FAQs", FAQs)
-app.use("/api/albums", albums)
-app.use("/api/descriptions", descriptions)
-app.use("/api/subscribers", subscribers)
-app.use("/api/announcements", announcements)
-app.use("/api/committees", committee)
-app.use("/api/feedback", feedback)
-app.use("/api/library", library)
-app.use("/api/messages", messages)
-app.use("/api/team_members", team_members)
-app.use("/api/achievements", achievements)
-app.use("/api/brief_descriptions", brief_description)
+const app = express()
 
-app.get("/", (req, res) => {
-  res.send(`<h1>Welcome to AWG Hub</h1>  `)
-})
+// DB Config
+const db = require("./config/keys").mongoURI
 
+// Connect to mongo
 mongoose
   .connect(db)
   .then(() => console.log("Connected to MongoDB"))
@@ -54,9 +21,14 @@ mongoose
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use((req, res) => {
-  res.status(404).send({ err: "We can not find what you are looking for" })
-})
+// Direct to Route Handlers
+app.use("/api/achievements", achievements)
+//app.use("/api/users", users)
+app.use("/api/clubmuns", clubmuns)
 
-const port = 5000
-app.listen(port, () => console.log(`Listening on port ${port}`))
+app.use((req, res) =>
+  res.status(404).send(`<h1>Can not find what you're looking for</h1>`)
+)
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server on ${port}`))
