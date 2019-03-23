d84e783a89dbe04d17dab3fdb488b340d6e7f571
habibagebely <habiba.elgebely@student.guc.edu.eg>
2019-03-23 21:26:52 +0200

    AP

--- routes/api/libraries.js
+++ routes/api/libraries.js
@@ -1,47 +1,60 @@
-// Dependencies
-const express = require('express');
-const Joi = require('joi');
+//Dependencies;
+const express = require("express");
+const Joi = require("joi");
 //const uuid = require('uuid');
 const router = express.Router();
 const app = express();
-app.use(express.json())
-const library=[
-    {academic_paper:"www.twitter.com",resolution:"wwww.facebook.com",year:"2017"},
-    {academic_paper:"www.youtube.com",resolution:"www.otlob.com",year:"2018"}
+app.use(express.json());
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-];
-router.post('/', (req, res) => { 
-    const academic_paper = req.body.academic_paper
-    const resolution = req.body.resolution
-    const year = req.body.year
-    const schema ={
-academic_paper : Joi.string().required(),
-resolution : Joi.string().required(),
-year:Joi.string().required()
-}
-const result = Joi.validate(req.body,schema);
-if(result.error){
-return res.status (400).send({error:result.error.details[0].message})}
+const Library = require("../../model/Library");
+const validator = require("../../Validations/LibraryValidations");
 
-    const paper = {
-        academic_paper: academic_paper,
-        resolution: resolution,
-        year: year,
-
+// done
+router.post("/", async (req, res) => {
+  const AP = new Library({
+    Academic_paper: req.body.Academic_paper,
+    Resolution: req.body.Resolution,
+    Year: req.body.Year
+  });
+  const isValidated = validator.createValidation(req.body);
+  try {
+    if (isValidated.error) {
+      return res
+        .status(400)
+        .send({ error: isValidated.error.details[0].message });
+    } else {
+      return AP.save().then(newAP => {
+        return res.status(201).json({
+          message: "New Acadimic paper was Uploaded successfully",
+          Library: newAP
+        });
+      });
     }
-    library.push(paper)
-    res.send(library)
-})
-router.get('/:year', (req, res) => { 
-    const paperyear = req.params.year
-    const paper = library.find(paper => paper.year === paperyear)
-    res.send(paper)
-})
-router.delete('/delete/:year', (req, res) => { //fine
-    const paperyear = req.params.year
-    const paper = library.find(paper => paper.year === paperyear)
-    const index = library.indexOf(paper)
-    library.splice(index,1)
-    res.send(library)
-})
-module.exports=router;
+  } catch (error) {
+    console.log(error);
+  }
+});
+//done
+router.get("/:year", async (req, res) => {
+  const Year = req.params.Year;
+  const AP = await Library.findOne(Year);
+  res.json({ data: AP });
+});
+
+//done
+router.delete("/:year", async (req, res) => {
+  try {
+    const inYear = req.params.Year;
+    const deletedAP = await Library.findOneAndDelete(inYear);
+    res.json({
+      msg: "Academic paper was deleted successfully",
+      data: deletedAP
+    });
+  } catch (error) {
+    // We will be handling the error later
+    console.log(error);
+  }
+});
+module.exports = router;
