349f310b4e3c4f546fc31c13cecd22df52f0517f
dina-elbeih <dina.elbeih@student.guc.edu.eg>
2019-03-30 20:56:22 +0200

    Added recruitment forms post method

--- routes/api/recruitmentforms.js
+++ routes/api/recruitmentforms.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const Joi = require("joi");
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
 const RecruitmentForm = require("../../models/RecruitmentForm");
-const validator = require("../../Validations/recruitmentformValidations");
+const validator = require("../../validations/recruitmentformValidations");
 
 router.get("/", async (req, res) => {
-    const message = await RecruitmentForm.find();
-    res.json({ data: message });
-  });
+  const message = await RecruitmentForm.find();
+  res.json({ data: message });
+});
 
+router.post("/", async (req, res) => {
+  try {
+    const isValidated = validator.createValidation(req.body);
+    if (isValidated.error)
+      return res
+        .status(400)
+        .send({ error: isValidated.error.details[0].message });
+    const newResponse = await RecruitmentForm.create(req.body);
+    res.json({ msg: "Your response has been saved", data: newResponse });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
