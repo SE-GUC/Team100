c28382dad3bde306675930b4f71bc2addd839aa2
dina-elbeih <dina.elbeih@student.guc.edu.eg>
2019-03-31 19:33:00 +0200

    Added albums tests

---
+++ fn/Albumfn.js
@@ -0,0 +1,42 @@
+const axios = require("axios");
+
+const functions = {
+  getAllAlbums: async () => {
+    const albums = await axios.get(`http://localhost:6000/api/albums/`);
+    return albums;
+  },
+  deleteCertainAlbum: async id => {
+    const album = await axios.delete(`http://localhost:6000/api/albums/${id}`);
+    return album;
+  },
+  updateCertainAlbum: async id => {
+    const album = await axios.put(`http://localhost:6000/api/albums/${id}`, {
+        title: "session 12",
+        description: "abcde",
+        photo: ["5c96009d01119d16989aebfd"]
+      });
+    return album;
+  },
+  postAlbum: async body => {
+    try {
+      const album = await axios.post(`http://localhost:6000/api/albums/`, {
+        title: "event 12",
+        description: "fcghbj",
+        photo: ["5c96009d01119d16989aebfc"]
+      });
+      console.log(album);
+      return album;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  getCertainAlbum: async id => {
+    try {
+      const album = await axios.get(`http://localhost:6000/api/albums/${id}`);
+      return album;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+module.exports = functions;
---
+++ tests/albums.test.js
@@ -0,0 +1,42 @@
+const funcs = require("../fn/Albumfn");
+
+//albums
+test("Get all albums", async done => {
+  expect.assertions(1);
+  const result = await funcs.getAllAlbums();
+  expect(result.data.data.length).toBe(3);
+});
+
+test("Get certain album", async done => {
+  expect.assertions(1);
+  const id = "5c95fed79c054043a42c9397";
+  const result = await funcs.getCertainAlbum(id);
+  expect(result.data.Album._id).toEqual(id);
+});
+
+test("Delete album", async done => {
+  expect.assertions(1);
+  const id = "5c95fed79c054043a42c9397";
+  const result = await funcs.deleteCertainAlbum(id);
+  expect(result.data.Album._id).toEqual(id);
+});
+
+test("Create album", async done => {
+  expect.assertions(3);
+  const result = await funcs.postAlbum();
+  expect(result.data.Album.title).toEqual("event 12");
+  expect(result.data.Album.description).toEqual("fcghbj");
+  expect(result.data.Album.photo).toEqual(["5c96009d01119d16989aebfc"]);
+  done();
+  expect(result.data.Album.uploaded_at).toEqual(Date.now);
+});
+
+test("Update album", async done => {
+  expect.assertions(3);
+  const id = "5c95fed79c054043a42c9397";
+  const result = await funcs.updateCertainAlbum(id);
+  expect(result.data.data.title).toEqual("session 12");
+  expect(result.data.data.description).toEqual("abcde");
+  expect(result.data.data.photo).toEqual(["5c96009d01119d16989aebfd"]);
+  done();
+});
