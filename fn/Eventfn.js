9e5a5287048e16970161b6cfedaf8cb68d516177
zyadkassab <zyad.kassab@student.guc.edu.eg>
2019-03-31 18:02:54 +0200

    testing events

--- fn/Eventfn.js
+++ fn/Eventfn.js
@@ -5,6 +5,25 @@ const functions = {
       "http://localhost:6000/api/events/timeline/f_soon"
     );
     return response;
+  },
+  getEventRate: async id => {
+    try {
+      const eventRate = await axios.get(
+        `http://localhost:6000/api/events/rate/${id}`
+      );
+      return eventRate;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  rateEvent: async () => {
+    const rate = await axios.put(
+      "http://localhost:6000/api/events/rate/5c9546969566a387d47f9001",
+      {
+        rate: 5
+      }
+    );
+    return rate;
   }
 };
 module.exports = functions;
--- tests/events.test.js
+++ tests/events.test.js
@@ -6,3 +6,21 @@ test("Get comming soon events", async done => {
   expect(response.data.data.length).toEqual(1);
   done();
 });
+test("get rate of an event", async done => {
+  expect.assertions(1);
+  id = "5c95468b9566a387d47f9000";
+  const rate = await funcs.getEventRate(id);
+  //console.log(achievement.data.Achievement._id);
+  expect(rate.data.Rate).toBe(1.3333333333333333);
+  done();
+});
+
+test("Rate a specific event", async done => {
+  expect.assertions(1);
+  ratings = await funcs.rateEvent();
+  expect(ratings.data.Rate).toBe(
+    ratings.data.Rating / ratings.data.Ratingcount
+  );
+  // expect(ach.data.Achievement.photo).toEqual("sprint33.com")
+  done();
+});
