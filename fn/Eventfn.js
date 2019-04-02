0a9a025c17f8d03425b16fbecbb50971b484a4e0
yasminhegazy <yasmin.abdelrahman@student.guc.edu.eg>
2019-04-02 19:47:19 +0200

    rest of event tests

--- fn/Eventfn.js
+++ fn/Eventfn.js
@@ -26,41 +26,57 @@ const functions = {
     return rate;
   },
   getCertainEvent: async (id) => {
-    try{
-        return response = await axios.get(`http://localhost:6000/api/events/${id}`);
-    }catch(error){
-        console.log(error);
+    try {
+      return response = await axios.get(`http://localhost:6000/api/events/${id}`);
+    } catch (error) {
+      console.log(error);
     }
   },
 
-deleteEvents: async(id) => {
-    try{
-        return response = await axios.delete(`http://localhost:6000/api/events/${id}`);
-    }catch(error){
-        console.log(error);
+  deleteEvents: async (id) => {
+    try {
+      return response = await axios.delete(`http://localhost:6000/api/events/${id}`);
+    } catch (error) {
+      console.log(error);
     }
-},
+  },
 
 
-updateEvents: async (id) => {
+  updateEvents: async (id) => {
     const ev = await axios.put(
       "http://localhost:6000/api/events/5c953831e4793067bcc3f233",
       {
         description: "update description",
-       
+
       }
     )
     return ev
   },
 
-createEvents: async(body) => {
-    try{
-        return response = await axios.post('http://localhost:6000/api/events/',body);
-    }catch(error){
-        console.log(error);
+  createEvents: async (body) => {
+    try {
+      return response = await axios.post('http://localhost:6000/api/events/', body);
+    } catch (error) {
+      console.log(error);
     }
-}
+  },
+  getEvent: async (id) => {
+    try {
+      id = "5c953831e4793067bcc3f233"
+      const response = await axios.get(`http://localhost:6000/api/events/eventbyid/${id}`);
+      return response;
+    }
+    catch (error) {
+      console.log(error);
+    }
+  },
 
+  currentEvents: async () => {
+    const response = await axios.get(
+      "http://localhost:6000/api/events/timeline/current"
+    );
+    return response;
+  }
 
 
 };
