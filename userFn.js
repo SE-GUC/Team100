75b1fe747daf4c0998d880f1131d3c7f2f4eaae8
yasminhegazy <yasmin.abdelrahman@student.guc.edu.eg>
2019-04-02 23:47:22 +0200

    users' tests

--- userFn.js
+++ userFn.js
@@ -24,6 +24,35 @@ const functions = {
             console.log(error);
         }
     },
+    updateUser: async(id,body)=>{
+        try{
+            const result= await axios.put(`http://localhost:6000/api/users/update_user/${id}`,body)
+            console.log(result);
+            return result
+        }catch (error){
+            console.log(error)
+        }
+
+    },
+    registeraUser: async (body) => {
+        try {
+
+            const u = await axios.post('http://localhost:6000/api/users/register/', body);
+            return u;
+        }
+        catch (error) {
+            console.log(error);
+        }
+    },
+    deleteUser: async (id) => {
+        try {
+            const u = await axios.delete(`http://localhost:6000/api/users/delete_user/${id}`);
+            return u
+        }
+        catch (error) {
+            console.log(error);
+        }
+    },
     
 }
 module.exports = functions;
--- users.test.js
+++ users.test.js
@@ -23,3 +23,46 @@ test("delete a user", async (done) => {
     expect(user.data.data._id).toEqual(id);
     done();
 })
+test("update User", async (done) => {
+    id = "5c94fbb3d50a3626620b38c9";
+    const u = {
+        name: "Mohamed"
+    };
+    const k = await funcs.updateUser(u);
+    const user = await funcs.getaUser(id)
+    expect(user.data.name).toEqual("Mohamed");
+    done();
+})
+
+test('add a new user', async (done) => {
+    expect.assertions(11)
+    const user = {
+        name: "Ahmed",
+        email: "ahm@gmail.com",
+        birth_date: " ",
+        password: "Ah12345",
+        major: "BI",
+        telephone: 243671351,
+        photo: "Photo1",
+        gucian: true,
+        club: " ",
+        committee_type: " ",
+        user_type: "user"
+    }
+    const u = await funcs.registeraUser(user);
+
+
+    expect(u.data.data.name).toEqual("Ahmed");
+    expect(u.data.User.email).toEqual("ahm@gmail.com");
+    expect(u.data.User.birth_date).toEqual(" ");
+    expect(u.data.User.password).toEqual("Ah12345");
+    expect(u.data.User.major).toEqual("BI");
+    expect(u.data.User.telephone).toEqual(243671351);
+    expect(u.data.User.photo).toEqual("Photo1");
+    expect(u.data.User.gucian).toEqual(true);
+    expect(u.data.User.club).toEqual(" ");
+    expect(u.data.User.committee_type).toEqual(" ");
+    expect(u.data.User.user_type).toEqual("user");
+
+    done();
+}, 100000);
