8004f20723de05563b2af17a42370dd5b5c289e5
mariamsalah119 <mariam.morsy@student.guc.edu.eg>
2019-04-04 20:35:30 +0200

    HOME PAGE

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import {BrowserRouter as Router, Route} from 'react-dom';
+//import Tools from './components/Tools';
+import Announcements from './components/announcements/Announcements';
+import SearchTool from './components/SearchTool';
+import Faqs from './components/faqs/Faqs';
+import './App.css';
+import Clubs from './components/clubs/Clubs';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Header />
+      <SearchTool />
+      <Clubs />
+      <Faqs />
+      <Announcements />
+      <Footer />
+
+  
+      </div>
+
+    );
+    
+  }
+
+
+}
+
+
+export default App;
---
+++ client/src/components/clubs/Clubs.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { AppRegistry, View } from 'react';
+
+
+class Clubs extends Component {
+
+
+  render() {
+
+    
+    return (
+        
+      <div>
+          
+      <h1>SlideShow</h1>
+      </div>
+    );
+  }
+}
+
+export default Clubs;
