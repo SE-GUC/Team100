29d085fecf16e7ac85ca51747eec7546205acfcf
dina-elbeih <dina.elbeih@student.guc.edu.eg>
2019-04-06 20:53:49 +0200

    Slideshow

--- client/src/components/clubs/Clubs.js
+++ client/src/components/clubs/Clubs.js
@@ -1,19 +1,65 @@
-import React, { Component } from 'react';
-import { AppRegistry, View } from 'react';
+import React, { Component } from "react";
+import { AppRegistry, View } from "react";
+import { Fade } from "react-slideshow-image";
 
+const fadeImages = [
+  "src/components/clubs/image2vector.svg"
+  // 'images/slide_3.jpg',
+  // 'images/slide_4.jpg'
+];
+
+const fadeProperties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: false,
+  indicators: true
+  //  arrows: true
+};
+
+const Slideshow = () => {
+  return (
+    <Fade {...fadeProperties}>
+    <h1>sdfghjkl</h1> 
+      <div className="each-fade">
+        <div className="image-container">
+          <img src={fadeImages[0]} />
+        </div>
+        <h2>First Slide</h2>
+      </div>
+      {/* <div className="each-slide">
+          <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+            <span>Slide 2</span>
+          </div>
+        </div>
+        <div className="each-slide">
+          <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+            <span>Slide 3</span>
+          </div>
+        </div> */}
+    </Fade>
+  );
+};
+
+function componentDidMount(){
+  console.log(fetch("/api/clubhub/"));
+}
 
 class Clubs extends Component {
-
-
   render() {
-
-    
-    return (
-        
-      <div>
-          
-      <h1>SlideShow</h1>
+    return( 
+      <Fade {...fadeProperties}>
+      <h1>Clubs</h1>
+      <div className="each-fade">
+        <div className="image-container">
+          <img src={fadeImages[0]} />
+        </div>
+        <h2>Slide 1</h2>
       </div>
+      </Fade>
+    // <div>
+
+    // <h1>SlideShow</h1>
+    // </div>
     );
   }
 }
