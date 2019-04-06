e56f61775727fd0b45533c1f5908ddc039431a05
Dohakaly <doha.aly@student.guc.edu.eg>
2019-04-06 19:27:33 +0200

    changes doha

--- server/routes/api/search.js
+++ server/routes/api/search.js
@@ -8,35 +8,35 @@ const Album = require("../../models/Album")
 const Event = require("../../models/Event")
 const Faq = require("../../models/Faq")
 
-router.get("/", async (req, res) => {
+router.get("/:keyword", async (req, res) => {
   try {
     const AchievementSearch = await Achievement.find(
-      { $text: { $search: req.body.search } },
+      { $text: { $search: req.params.keyword } },
       { score: { $meta: "textScore" } }
     ).sort({ score: { $meta: "textScore" } })
 
     const AlbumSearch = await Album.find(
-      { $text: { $search: req.body.search } },
+      { $text: { $search: req.params.keyword } },
       { score: { $meta: "textScore" } }
     ).sort({ score: { $meta: "textScore" } })
 
     const AnnouncementSearch = await Announcement.find(
-      { $text: { $search: req.body.search } },
+      { $text: { $search: req.params.keyword } },
       { score: { $meta: "textScore" } }
     ).sort({ score: { $meta: "textScore" } })
 
     const ClubhubSearch = await Club_hub.find(
-      { $text: { $search: req.body.search } },
+      { $text: { $search: req.params.keyword } },
       { score: { $meta: "textScore" } }
     ).sort({ score: { $meta: "textScore" } })
 
     const FaqSearch = await Faq.find(
-      { $text: { $search: req.body.search } },
+      { $text: { $search: req.params.keyword } },
       { score: { $meta: "textScore" } }
     ).sort({ score: { $meta: "textScore" } })
 
     const eventSearch = await Event.find(
-      { $text: { $search: req.body.search } },
+      { $text: { $search: req.params.keyword } },
       { score: { $meta: "textScore" } }
     ).sort({ score: { $meta: "textScore" } })
 
