75cee4de9d101e3536b5320c488d2f090d3a4e18
yomna-sherif”git config --global user.email yomna.abdelallah@student.guc.edu.eg” <yomna.abdelallah@student.guc.edu.eg>
2019-04-07 13:14:25 +0200

    slideshow description

--- client/src/components/clubs/Clubs.js
+++ client/src/components/clubs/Clubs.js
@@ -1,14 +1,49 @@
-import React, { Component } from "react"
-//import { AppRegistry, View } from 'react';
+import React, { Component } from "react";
+import { AppRegistry, View } from "react";
+import { Fade } from "react-slideshow-image";
+
+
+const fadeProperties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: false,
+  indicators: true
+};
 
 class Clubs extends Component {
+  constructor() {
+    super();
+    this.state = {
+      club_hub: []
+    };
+  }
+
+  componentDidMount() {
+    this.refreshClubs();
+  }
+
+  refreshClubs() {
+    fetch("/api/club_hub")
+      .then(res => res.json())
+      .then(club_hub => {
+        console.log("clubs fetched..", club_hub);
+        this.setState({ club_hub: club_hub.data });
+      });
+  }
+
   render() {
     return (
-      <div>
-        <h1>SlideShow</h1>
-      </div>
-    )
+      <Fade {...fadeProperties}>
+        {this.state.club_hub.map((each, index) => (
+          <div>
+  
+            <p>{each.brief_description}</p>
+          </div>
+        ))}
+      </Fade>
+    );
   }
 }
 
-export default Clubs
+export default Clubs;
+
