c734713f4f141618d02e3a371aa1bb58d71bce65
montasserr <marwan.montasser@student.guc.edu.eg>
2019-04-07 20:46:30 +0200

    Delete FAQs

--- client/src/components/faqs/Faqs.js
+++ client/src/components/faqs/Faqs.js
@@ -56,10 +56,18 @@ class Faqs extends Component {
     console.log(updatedFaq);
     try {
       await axios.put(`faqs/${faq.target.getAttribute("data-index")}`, updatedFaq);
+      this.refreshFaqs();
     }
     catch (error) {
       console.log(error);
     }
+  };
+
+onDelete = e => {
+    // console.log(e.target.getAttribute("data-index"))
+    fetch(`/api/faqs/${e.target.getAttribute("data-index")}`, {
+      method: "DELETE"
+    }).then(res => this.refreshFaqs());
   };
 
   handleS = async Faqs => {
@@ -72,6 +80,7 @@ class Faqs extends Component {
     console.log(faq);
     try {
       await axios.post(`FAQs/`, faq);
+      this.refreshFaqs();
     } catch (error) {
       console.log(error);
     }
@@ -88,6 +97,9 @@ class Faqs extends Component {
                 <li>
                   {faq.question} {faq.answer}
                 </li>
+                <button onClick={this.onDelete} data-index={faq._id}>
+                  DELETE
+                </button>
                 <form onSubmit={this.handleSubmit} data-index={faq._id}>
                   Q:<input type="text" name="question" defaultValue={faq.question} />
                   A:<input type="text" name="answer" defaultValue={faq.answer} />
