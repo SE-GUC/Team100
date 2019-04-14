94c607850c15e6bdf20035200b208821e2fa4a4f
habibagebely <habiba.elgebely@student.guc.edu.eg>
2019-04-14 19:59:44 +0200

    view

--- client/src/componentsMun/events/Events.js
+++ client/src/componentsMun/events/Events.js
@@ -115,47 +115,47 @@ class Events extends Component {
     this.setState({ committee: event.target.value });
   };
 
-//   refreshRates() {
-//     axios.get("http://localhost:9000/api/events").then(res => {
-//       this.setState({ Events: res.data.data });
-//       console.log(res.data.data);
-//     });
-//   }
+  refreshRates() {
+    axios.get("http://localhost:9000/api/events").then(res => {
+      this.setState({ Events: res.data.data });
+      console.log(res.data.data);
+    });
+  }
 
-//   onCreateRate = async Events => {
-//     Events.preventDefault();
+  onCreateRate = async Events => {
+    Events.preventDefault();
 
-//     const event = {
-//       rate: this.state.rate
-//     };
-//     console.log(event);
-//     try {
-//       await axios.put(
-//         `events/${event.target.getAttribute("data-index")}`
-//         //updatedEvent
-//       );
-//       this.refreshEvents();
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-//   onCreateFeed = async Events => {
-//     Events.preventDefault();
+    const event = {
+      rate: this.state.rate
+    };
+    console.log(event);
+    try {
+      await axios.put(
+        `events/${event.target.getAttribute("data-index")}`
+        //updatedEvent
+      );
+      this.refreshEvents();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  onCreateFeed = async Events => {
+    Events.preventDefault();
 
-//     const event = {
-//       feedback: this.state.feed
-//     };
-//     console.log(event);
-//     try {
-//       await axios.put(
-//         `events/${event.target.getAttribute("data-index")}`
-//         //updatedEvent
-//       );
-//       this.refreshEvents();
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
+    const event = {
+      feedback: this.state.feed
+    };
+    console.log(event);
+    try {
+      await axios.put(
+        `events/${event.target.getAttribute("data-index")}`
+        //updatedEvent
+      );
+      this.refreshEvents();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   onCreate = async Events => {
     Events.preventDefault();
@@ -359,7 +359,7 @@ class Events extends Component {
           </ul>
         }
 
-        {/* <Collapsible trigger="Give a feedback">
+        <Collapsible trigger="Give a feedback">
           <form onSubmit={this.onCreateFeed}>
             <label>
               Event name:
@@ -403,7 +403,7 @@ class Events extends Component {
               {eve.rate}
             </li>
           </div>
-        ))} */}
+        ))}
 
         <h3>Events timeline</h3>
         <Timeline>
